08a292d6c4e25f08e27fedff1d0e28bd08c5e47f
okamotchan <m.okamotchan@gmail.com>
2019-05-19 23:40:08 +0900

    validate追加

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -7,7 +7,11 @@
 #  price      :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  genre      :string
 #
 
 class Expense < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :genre, presence: true
 end
